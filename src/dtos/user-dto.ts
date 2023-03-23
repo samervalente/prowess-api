e07fdd428bfd50ca3d93266f59ea4f9212ec8b63
samervalente <samervalente@gmail.com>
2023-03-23 19:41:20 -0300

    feat: create CreateUserDTO

---
+++ src/dtos/user-dto.ts
@@ -0,0 +1,23 @@
+import { IsString, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsNotEmpty()
+  @IsString()
+  first_name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  surname: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty({ message: 'password should not be empty' })
+  @MinLength(3, { message: 'password must have at least 3 characters' })
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  birthDate: string | Date;
+}
