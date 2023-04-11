97fb94bdafb3a623adfba000ee67c9bd5e2e6438
samervalente <samervalente@gmail.com>
2023-04-11 12:07:30 -0300

    feat: create UserViewModel

---
+++ src/infra/http/view-models/user-view-model.ts
@@ -0,0 +1,14 @@
+import { User } from "@prisma/client";
+
+export class UserViewModel {
+    static toHTTP(user: User){
+        return {
+            id: user.id,
+            name: user.name,
+            email:user.email,
+            password: user.password,
+            birthDate:user.birthDate,
+            createdAt: user.createdAt,
+        }
+    }
+}
