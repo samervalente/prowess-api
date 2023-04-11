db6772c34ab0e8b3307342c4fa47990895cf7165
samervalente <samervalente@gmail.com>
2023-04-11 10:50:28 -0300

    feat: add create-user use-case

--- src/app/entities/user/use-cases/create-user.ts
+++ src/app/entities/user/use-cases/create-user.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { User, UserProps } from "../user";
+import { User } from "../user";
 import { UserRepository } from "../user-repository";
 import { CreateUserDTO } from "src/infra/http/dtos/create-user-dto";
 
@@ -14,7 +14,10 @@ export class CreateUser {
 
     async handle(request: CreateUserDTO): Promise<User> {
         const {first_name, surname, email, password, birthDate} = request
-        const user = new User(first_name, surname, email, password, birthDate);
-        const {}
+        const name = first_name + surname
+        const user = new User({name, email, password, birthDate});
+        
+        const createdUser = await this.userRepository.create(user)
+        return createdUser
     }
 }
--- src/app/entities/user/user-repository.ts
+++ src/app/entities/user/user-repository.ts
@@ -1,10 +1,9 @@
 import { Injectable } from "@nestjs/common";
-import { CreateUserDTO } from "src/infra/http/dtos/create-user-dto";
 import { User } from "./user";
 
 
 @Injectable()
 
 export abstract class UserRepository{
-    abstract create(user: CreateUserDTO): Promise<User>
+    abstract create(user: User): Promise<User>
 }
