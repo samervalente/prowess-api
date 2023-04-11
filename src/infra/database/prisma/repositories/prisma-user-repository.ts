b7c7140a7c59138b94063e2d8bf1380b71bd1b1f
samervalente <samervalente@gmail.com>
2023-04-11 11:24:23 -0300

    feat: create prisma user repository

---
+++ src/infra/database/prisma/repositories/prisma-user-repository.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { UserRepository } from "src/app/entities/user/user-repository";
+import { PrismaService } from "../prisma.service";
+import { User } from "src/app/entities/user/user";
+import { PrismaUserMapper } from "../mappers/prisma-user-mapper";
+
+
+@Injectable()
+export class PrismaUserRepository implements UserRepository{
+    constructor(private prisma: PrismaService) {
+    }
+
+    async create(user: User): Promise<User> {
+        const raw = PrismaUserMapper.toPrisma(user);
+        const createdUser = await this.prisma.user.create({data: raw})
+        return PrismaUserMapper.toDomain(createdUser)
+    }
+}
